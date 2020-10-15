ca957bcd9433eba76a21125ac4e93d30770c16ee
micolka <mic_la@tut.by>
2020-10-16 00:02:13 +0300

    feat: make letter, number, return, backspase, space work

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -13,8 +13,9 @@ class VirtualKeyboard {
     this.keyConfig = keyConfig;
   }
 
-  init(initialValue) {
-    this.value = initialValue;
+  init(textArea) {
+    this.textArea = textArea;
+    this.value = textArea.value;
     this._buildKeyboard();
   }
 
@@ -33,21 +34,85 @@ class VirtualKeyboard {
   _createKeys() {
     const fragment = document.createDocumentFragment();
 
+    const createIconHTML = (icon) => {
+      return `<i class="material-icons">${icon}</i>`
+    }
+
     for (let el in this.keyConfig) {
       let key = document.createElement('button');
       key.classList.add('keyboard__key');
       key.textContent = this.keyConfig[el];
+      key.type = 'button';
+
+      switch (key.textContent) {
+
+        case `backspace`:
+          key.classList.add('keyboard__key--wide');
+          key.innerHTML = createIconHTML('backspace');
+
+          key.addEventListener('click', () => {
+            this.removeSymbol();
+          })
+          break;
+
+        case `space`:
+          key.classList.add('keyboard__key--extra-wide');
+          key.innerHTML = createIconHTML('space_bar');
+          key.addEventListener('click', () => {
+            this.addSymbol(' ');
+          })  
+          break;
+
+        case `caps`:
+          key.classList.add('keyboard__key--wide', 'keyboard__key--activatable');
+          key.innerHTML = createIconHTML('keyboard_capslock');
+          break;
+
+        case `done`:
+          key.classList.add('keyboard__key--wide', 'keyboard__key--dark');
+          key.innerHTML = createIconHTML('check_circle');
+          break;  
+
+        case `enter`:
+          key.classList.add('keyboard__key--wide');
+          key.innerHTML = createIconHTML('keyboard_return');
+
+          key.addEventListener('click', () => {
+            this.addSymbol('\n');
+          })  
+          break;
+
+        default:
+          key.addEventListener('click', () => {
+            this.addSymbol(key.textContent);
+          })  
+      }
       fragment.appendChild(key);
+
+      if (key.textContent === 'backspace' || key.textContent === 'p' 
+            || key.textContent === 'enter' || key.textContent === '?') {
+        fragment.appendChild(document.createElement('br'));
+      }
     }
 
     return fragment;
+  }
 
+  addSymbol(symbol) {
+    this.value += symbol;
+    this.textArea.value = this.value;
+  }
+
+  removeSymbol() {
+    this.value = this.value.slice(0, this.value.length - 1);
+    this.textArea.value = this.value;
   }
 
 }
 
 const a4Tech = new VirtualKeyboard(keyConfig);
+let textArea = document.querySelector('.use-keyboard-input');
 
 window.addEventListener('DOMContentLoaded', function() {
-  a4Tech.init();
+  a4Tech.init(textArea);
 }) 
