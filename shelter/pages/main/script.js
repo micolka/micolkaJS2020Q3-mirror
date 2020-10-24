6a2b4630df370760b45d3e3b3709c1810f84ca59
micolka <mic_la@tut.by>
2020-10-24 19:26:13 +0300

    feat: end with slider

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -46,12 +46,14 @@ let carousel = function () {
   }
 
   function previousItem(n) {
+    generatePrevCard()
     hideItem('to-right');
     changeCurrentItem(n - 1);
     showItem('from-left');
   }
 
   function nextItem(n) {
+    generateNextCard();
     hideItem('to-left');
     changeCurrentItem(n + 1);
     showItem('from-right');
@@ -71,4 +73,47 @@ let carousel = function () {
 
 }
 
+let currentPetsIndexes = []
+let currentCard = 0;
+let petsCardArr = document.querySelectorAll('.pets-card');
+// Fills pets cards
+function fillPetsInfo() {
+  let pseudoIndexes = genPseudoRandom(currentPetsIndexes);
+  for (let i = 0; i < 3; i ++) {
+    let index = pseudoIndexes[i]
+    petsCardArr[i + currentCard].children[0].src = petsData[index].img
+    petsCardArr[i + currentCard].children[1].textContent = petsData[index].name
+    currentPetsIndexes.push(index)
+  }
+}
+
+function genPseudoRandom(current) {
+  let arr = [0,1,2,3,4,5,6,7];
+  let out = [];
+  for(let i = 8; i > 0; i--) {
+    let indx = Math.floor(Math.random() * i);
+    out.push(arr[indx]);
+    arr.splice(indx, 1);
+  }
+  if (current.length !== 0) {
+    for(let i = 0; i < 3; i++) {
+      out.splice(out.indexOf(current[i]), 1);
+    }
+  }
+  currentPetsIndexes = []
+  return out;
+}
+
+function generateNextCard() {
+  currentCard += 3
+  if (currentCard > 6) currentCard = 0
+  fillPetsInfo(currentPetsIndexes)
+}
+function generatePrevCard() {
+  currentCard -= 3
+  if (currentCard < 0) currentCard = 6
+  fillPetsInfo(currentPetsIndexes)
+}
+
+fillPetsInfo();
 carousel();
