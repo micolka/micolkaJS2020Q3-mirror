7487f93e11fcb9ad3b2c1e1ec60c4b8664b97469
micolka <mic_la@tut.by>
2020-10-30 01:28:28 +0300

    feat: implement shift-key and lang change

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -1,18 +1,44 @@
-const keyConfig = [
-  "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-  "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-  "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-  "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
-  "space"
-];
-
+const keyConfig = {
+  en: [
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace",
+    "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]",
+    "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "enter",
+    "shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/",
+    "done", "space", "en / ru"
+  ],
+  enShifted: [
+    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'skip',
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}',
+    'skip', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'skip',
+    'skip', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?',
+    'skip', 'skip', 'skip'
+  ],
+  ru: [
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "skip",
+    "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ",
+    "skip", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "skip",
+    "skip", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".",
+    "skip", "skip", "skip"
+  ],
+  ruShifted: [
+    "!", '"', "№", ";", "%", ":", "?", "*", "(", ")", "_", "+", "skip",
+    "Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ",
+    "skip", "Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э", "skip",
+    "skip", "Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю", ",",
+    "skip", "skip", "skip"
+  ],
+}
+// TO DO fix bug with shift/lang mixing
 
 class VirtualKeyboard {
   constructor(keyConfig) {
     this.value = '';
+    this.engKeyConfig = keyConfig.en;
     this.keyConfig = keyConfig;
     this.cursorPosition = 0;
     this.isCaps = false;
+    this.isShifted = false;
+    this.language = 'en';
   }
 
   init() {
@@ -26,7 +52,7 @@ class VirtualKeyboard {
     let keyboard = document.createElement('div');
     let keyboardKeys = document.createElement('div');
 
-    keyboard.classList.add('keyboard', 'keyboard--hidden');
+    keyboard.classList.add('keyboard');//, 'keyboard--hidden');
     keyboardKeys.classList.add('keyboard__keys');
     keyboardKeys.appendChild(this._createKeys());
 
@@ -43,10 +69,10 @@ class VirtualKeyboard {
       return `<i class="material-icons">${icon}</i>`
     }
 
-    for (let el in this.keyConfig) {
+    for (let el in this.engKeyConfig) {
       let key = document.createElement('button');
       key.classList.add('keyboard__key');
-      key.textContent = this.keyConfig[el];
+      key.textContent = this.engKeyConfig[el];
       key.type = 'button';
 
       switch (key.textContent) {
@@ -69,6 +95,30 @@ class VirtualKeyboard {
             this.addSymbol(' ');
           })
 
+          break;
+
+        case `shift`:
+          key.classList.add('keyboard__key--wide', 'keyboard__key--activatable');
+          key.textContent = 'Shift'
+
+          key.addEventListener('click', () => {
+            this._shiftKeys();
+            this._toggleShift();
+            key.classList.toggle('keyboard__key--active');
+          });
+          
+          break;
+
+        case `en / ru`:
+          key.classList.add('keyboard__key--wide');
+          key.textContent = 'EN / ru';
+
+          key.addEventListener('click', () => {
+            key.textContent === 'EN / ru' ? key.textContent = 'en / RU' : key.textContent = 'EN / ru';
+            this._shiftLang();
+            this._toggleLang();
+          });
+          
           break;
 
         case `caps`:
@@ -116,13 +166,47 @@ class VirtualKeyboard {
       }
       fragment.appendChild(key);
 
-      if (key.textContent === 'backspace' || key.textContent === 'p' 
-            || key.textContent === 'keyboard_return' || key.textContent === '?') {
+      if (key.textContent === 'backspace' || key.textContent === ']' 
+            || key.textContent === 'keyboard_return' || key.textContent === '/') {
         fragment.appendChild(document.createElement('br'));
       }
     }
 
     return fragment;
+  }
+
+  _shiftKeys() {
+    document.querySelectorAll('.keyboard__key').forEach( (el, i) => {
+      let tmp = el.textContent
+      if (this.language === 'en') {
+        if ( this.keyConfig.enShifted[i] !== 'skip' ) {
+          el.textContent = this.keyConfig.enShifted[i];
+          this.keyConfig.enShifted[i] = tmp;
+        }
+      } else {
+        if ( this.keyConfig.ruShifted[i] !== 'skip' ) {
+          el.textContent = this.keyConfig.ruShifted[i];
+          this.keyConfig.ruShifted[i] = tmp;
+        }
+      }
+    })
+  }
+
+  _shiftLang() {
+    document.querySelectorAll('.keyboard__key').forEach( (el, i) => {
+      let tmp = el.textContent
+      if (this.isShifted) {
+        if ( this.keyConfig.ruShifted[i] !== 'skip' ) {
+          el.textContent = this.keyConfig.ruShifted[i];
+          this.keyConfig.ruShifted[i] = tmp;
+        }
+      } else {
+        if ( this.keyConfig.ru[i] !== 'skip' ) {
+          el.textContent = this.keyConfig.ru[i];
+          this.keyConfig.ru[i] = tmp;
+        }
+      }
+    })
   }
 
   _setTextAreaBindings() {
@@ -147,6 +231,14 @@ class VirtualKeyboard {
 
   _toggleCaps() {
     this.isCaps ? this.isCaps = false : this.isCaps = true; 
+  }  
+
+  _toggleShift() {
+    this.isShifted ? this.isShifted = false : this.isShifted = true; 
+  }
+
+  _toggleLang() {
+    this.language === 'en' ? this.language = 'ru' : this.language = 'en';
   }
 
   addSymbol(symbol) {
