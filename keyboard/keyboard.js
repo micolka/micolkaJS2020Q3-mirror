c1a08886dd378a3bca003ad8d9b069fbd2421689
micolka <mic_la@tut.by>
2020-10-29 21:31:04 +0300

    feat: implement capslock, cursor position tracking

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -11,24 +11,29 @@ class VirtualKeyboard {
   constructor(keyConfig) {
     this.value = '';
     this.keyConfig = keyConfig;
+    this.cursorPosition = 0;
+    this.isCaps = false;
   }
 
-  init(textArea) {
-    this.textArea = textArea;
-    this.value = textArea.value;
-    this._buildKeyboard();
+  init() {
+    this.textArea = document.querySelector('.use-keyboard-input');
+    this.value = this.textArea.value;
+    this.keyboard = this._buildKeyboard();
+    this._setTextAreaBindings();
   }
 
   _buildKeyboard() {
     let keyboard = document.createElement('div');
     let keyboardKeys = document.createElement('div');
 
-    keyboard.classList.add('keyboard');
+    keyboard.classList.add('keyboard', 'keyboard--hidden');
     keyboardKeys.classList.add('keyboard__keys');
     keyboardKeys.appendChild(this._createKeys());
 
     keyboard.appendChild(keyboardKeys);
     document.querySelector('body').append(keyboard);
+
+    return keyboard;
   }
 
   _createKeys() {
@@ -53,24 +58,45 @@ class VirtualKeyboard {
           key.addEventListener('click', () => {
             this.removeSymbol();
           })
+
           break;
 
         case `space`:
           key.classList.add('keyboard__key--extra-wide');
           key.innerHTML = createIconHTML('space_bar');
+
           key.addEventListener('click', () => {
             this.addSymbol(' ');
-          })  
+          })
+
           break;
 
         case `caps`:
           key.classList.add('keyboard__key--wide', 'keyboard__key--activatable');
           key.innerHTML = createIconHTML('keyboard_capslock');
+
+          key.addEventListener('click', () => {
+            document.querySelectorAll('.keyboard__key').forEach( el => {
+              if (this.isCaps) {
+                if (el.textContent.length === 1) el.textContent = el.textContent.toLowerCase();
+              } else {
+                if (el.textContent.length === 1) el.textContent = el.textContent.toUpperCase();
+              }
+            });
+            this._toggleCaps();
+            key.classList.toggle('keyboard__key--active');
+          });
+
           break;
 
         case `done`:
           key.classList.add('keyboard__key--wide', 'keyboard__key--dark');
           key.innerHTML = createIconHTML('check_circle');
+
+          key.addEventListener('click', () => {
+            this.close();
+          });
+
           break;  
 
         case `enter`:
@@ -80,6 +106,7 @@ class VirtualKeyboard {
           key.addEventListener('click', () => {
             this.addSymbol('\n');
           })  
+
           break;
 
         default:
@@ -90,7 +117,7 @@ class VirtualKeyboard {
       fragment.appendChild(key);
 
       if (key.textContent === 'backspace' || key.textContent === 'p' 
-            || key.textContent === 'enter' || key.textContent === '?') {
+            || key.textContent === 'keyboard_return' || key.textContent === '?') {
         fragment.appendChild(document.createElement('br'));
       }
     }
@@ -98,21 +125,58 @@ class VirtualKeyboard {
     return fragment;
   }
 
+  _setTextAreaBindings() {
+    this.textArea.addEventListener('focus', () => {
+      a4Tech.open();
+    })
+
+    this.textArea.addEventListener('input', () => {
+      this.value = this.textArea.value;
+    })
+
+    this.textArea.addEventListener('click', () => {
+      this.cursorPosition = this.textArea.selectionStart;
+    })
+  }
+
+  _textAreaReturnFocus() {
+    this.textArea.selectionStart = this.cursorPosition;
+    this.textArea.selectionEnd = this.cursorPosition;
+    this.textArea.focus();
+  }
+
+  _toggleCaps() {
+    this.isCaps ? this.isCaps = false : this.isCaps = true; 
+  }
+
   addSymbol(symbol) {
-    this.value += symbol;
+    this.value = this.value.slice(0, this.cursorPosition) 
+      + symbol + this.value.slice(this.cursorPosition);
+    this.cursorPosition++;
     this.textArea.value = this.value;
+    this._textAreaReturnFocus();
   }
 
   removeSymbol() {
-    this.value = this.value.slice(0, this.value.length - 1);
+    this.value = this.value.slice(0, this.cursorPosition - 1) 
+      + this.value.slice(this.cursorPosition);
+    this.cursorPosition--;
     this.textArea.value = this.value;
+    this._textAreaReturnFocus();
+  }
+
+  open() {
+    this.keyboard.classList.remove('keyboard--hidden');
+  }
+
+  close() {
+    this.keyboard.classList.add('keyboard--hidden');
   }
 
 }
 
 const a4Tech = new VirtualKeyboard(keyConfig);
-let textArea = document.querySelector('.use-keyboard-input');
 
 window.addEventListener('DOMContentLoaded', function() {
-  a4Tech.init(textArea);
+  a4Tech.init();
 }) 
