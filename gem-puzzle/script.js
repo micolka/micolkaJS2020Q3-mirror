6f33e6d473ec566aab4be8cca956e70158bccfa4
micolka <mic_la@tut.by>
2020-11-06 22:20:11 +0300

    feat: implement save\load functions and field size change

--- gem-puzzle/script.js
+++ gem-puzzle/script.js
@@ -1,11 +1,13 @@
 class GemPuzzle {
   constructor() {
-    this.turnsCount = 0;
     this.tileSize = 80;
     this.tileGap = 10;
     this.fieldSize = 4;
     this.xOffset = 10;
     this.yOffset = 10;
+    this.turnsCount = 0;
+    this.initTimeValue = 0;
+    this.duration = 0;
   }
 
   init() {
@@ -13,21 +15,23 @@ class GemPuzzle {
     this.field = this.createTileFiled();
     this.stats = this.createStatsPanel();
     this.newGame();
-    this.gameTimer = document.querySelector('.game-timer');
+    this.gameTimer = document.querySelector('.game_timer');
   }
 
+  // Create field of tiles
   createTileFiled() {
-    // Create field
-    const field = document.createElement('div');
-    field.classList.add('field');
-
     const {
       tileSize, tileGap, fieldSize, yOffset, xOffset,
     } = this;
 
+    const field = document.createElement('div');
+    field.classList.add('field');
+    field.style.width = `${(tileGap + tileSize) * fieldSize + tileGap}px`;
+    field.style.height = field.style.width;
+
     let j = -1;
     for (let i = 0; i < fieldSize ** 2; i += 1) {
-      // Create tile and position it on the field
+      // Create tile and place it on the field
       const tile = document.createElement('div');
       tile.classList.add('tile');
       tile.style.left = `${xOffset + (i % fieldSize) * (tileSize + tileGap)}px`;
@@ -39,10 +43,7 @@ class GemPuzzle {
       } else {
         tile.classList.add('empty');
       }
-      // Add event listeners for each tile
-      // tile.addEventListener('click', (event) => {
-      //   this.shiftTiles(event.currentTarget);
-      // });
+
       tile.addEventListener('mousedown', (event) => {
         this.grabTile(event);
       });
@@ -51,21 +52,26 @@ class GemPuzzle {
       field.appendChild(tile);
     }
 
-    this.body.appendChild(field);
+    if (this.field) {
+      this.body.replaceChild(field, this.field);
+    } else {
+      this.body.appendChild(field);
+    }
 
     return field;
   }
 
   // Shift tile with and empty space if it nearby
   shiftTiles(target) {
+    const tile = target;
     const { top, left } = target.style;
 
     const emptyChildIndex = this.fieldSize ** 2 - 1;
     const emptyChild = this.field.childNodes[emptyChildIndex];
 
     if (this.isTargetNearEmptySpace(target)) {
-      target.style.top = emptyChild.style.top;
-      target.style.left = emptyChild.style.left;
+      tile.style.top = emptyChild.style.top;
+      tile.style.left = emptyChild.style.left;
 
       emptyChild.style.left = left;
       emptyChild.style.top = top;
@@ -75,12 +81,12 @@ class GemPuzzle {
 
   updateCounter() {
     this.turnsCount += 1;
-    this.stats.innerText = this.turnsCount;
+    this.stats.innerText = `Turns: ${this.turnsCount}`;
   }
 
   resetCounter() {
     this.turnsCount = 0;
-    this.stats.innerText = this.turnsCount;
+    this.stats.innerText = `Turns: ${this.turnsCount}`;
   }
 
   isTargetNearEmptySpace(target) {
@@ -100,18 +106,18 @@ class GemPuzzle {
     clearInterval(this.timer);
     this.timer = setInterval(() => {
       const now = new Date().getTime();
-      const distance = now - countDate;
+      this.duration = now - countDate + this.initTimeValue;
 
       function addZero(param) {
         const result = param < 10 ? `0${param}` : param;
         return result;
       }
 
-      const hours = addZero(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-      const minutes = addZero(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
-      const seconds = addZero(Math.floor((distance % (1000 * 60)) / 1000));
+      const hours = addZero(Math.floor((this.duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      const minutes = addZero(Math.floor((this.duration % (1000 * 60 * 60)) / (1000 * 60)));
+      const seconds = addZero(Math.floor((this.duration % (1000 * 60)) / 1000));
 
-      this.gameTimer.innerHTML = `Time:${hours}:${minutes}:${seconds}`;
+      this.gameTimer.innerHTML = `Time: ${hours}:${minutes}:${seconds}`;
     }, 1000);
   }
 
@@ -119,36 +125,96 @@ class GemPuzzle {
   createStatsPanel() {
     const panel = document.createElement('div');
     panel.classList.add('stats-panel');
+    // Turns counter
     panel.innerText = this.turnsCount;
-
+    // Game timer
     const timer = document.createElement('span');
-    timer.classList.add('game-timer');
+    timer.classList.add('game_timer');
+    // New game button
+    const newGame = document.createElement('button');
+    newGame.classList.add('new_game-button');
+    newGame.innerText = 'New Game';
+    newGame.addEventListener('click', () => {
+      this.newGame();
+    });
+    // Save game button
+    const saveGame = document.createElement('button');
+    saveGame.classList.add('save_game-button');
+    saveGame.innerText = 'Save Game';
+    saveGame.addEventListener('click', () => {
+      this.saveGame();
+    });
+    // Save game button
+    const loadGame = document.createElement('button');
+    loadGame.classList.add('load_game-button');
+    loadGame.innerText = 'Load Game';
+    loadGame.addEventListener('click', () => {
+      this.loadGame();
+    });
 
-    const shuffle = document.createElement('button');
-    shuffle.classList.add('shuffle-button');
-    shuffle.innerText = 'New Game';
-    shuffle.addEventListener('click', () => {
+    const select = document.createElement('select');
+    for (let i = 3; i <= 8; i += 1) {
+      const option = document.createElement('option');
+      option.value = i;
+      if (i === 4) option.selected = true;
+      option.innerText = `${i}x${i}`;
+      select.appendChild(option);
+    }
+    select.addEventListener('change', () => {
+      this.fieldSize = select.value;
+      this.field = this.createTileFiled();
       this.newGame();
     });
 
     this.body.appendChild(timer);
     this.body.appendChild(panel);
-    this.body.appendChild(shuffle);
+    this.body.appendChild(newGame);
+    this.body.appendChild(saveGame);
+    this.body.appendChild(loadGame);
+    this.body.appendChild(select);
 
     return panel;
   }
 
   // Move tiles in random order
   shuffleTiles() {
-    for (let i = 0; i < 1000; i += 1) {
+    for (let i = 0; i < 5000 * this.fieldSize; i += 1) {
       const index = Math.floor(Math.random() * (this.fieldSize ** 2 - 1));
       this.shiftTiles(this.field.childNodes[index]);
     }
   }
 
+  // Resets counter, timer and generates new field
   newGame() {
     this.shuffleTiles();
     this.resetCounter();
+    this.duration = 0;
+    this.initTimeValue = 0;
+    this.runGameTimer(new Date().getTime());
+  }
+
+  // Save game status into local storage
+  saveGame() {
+    const acc = [];
+    this.field.childNodes.forEach((el) => {
+      acc.push(el.style.cssText);
+    });
+    localStorage.setItem('save', JSON.stringify(acc));
+    localStorage.setItem('turns', JSON.stringify(this.turnsCount));
+    localStorage.setItem('duration', JSON.stringify(this.duration));
+  }
+
+  // Load game status from local storage
+  loadGame() {
+    const acc = JSON.parse(localStorage.getItem('save'));
+    this.field.childNodes.forEach((el, index) => {
+      el.style.cssText = acc[index];
+    });
+
+    this.turnsCount = JSON.parse(localStorage.getItem('turns'));
+    this.stats.innerText = `Turns: ${this.turnsCount}`;
+    this.initTimeValue = JSON.parse(localStorage.getItem('duration'));
+    this.duration = 0;
     this.runGameTimer(new Date().getTime());
   }
 
@@ -189,7 +255,6 @@ class GemPuzzle {
         target.style.left = left;
       }
       target.style.zIndex = 'auto';
-
       this.updateCounter();
       target.onmouseup = null;
     }
