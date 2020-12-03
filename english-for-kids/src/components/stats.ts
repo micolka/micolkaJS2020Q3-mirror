26a0fccccedb90c54a611150a8dc2c44872a89de
micolka <mic_la@tut.by>
2020-12-04 00:33:12 +0300

    feat: add sort function to stats table

--- english-for-kids/src/components/stats.ts
+++ english-for-kids/src/components/stats.ts
@@ -3,6 +3,43 @@ import state from '../appState';
 import cards from '../cardsConfig';
 import { getRootElement } from '../utils';
 
+function addEventListeners() {
+  const table = document.querySelector('.stats_table');
+  const tableHeaders = document.querySelectorAll('th');
+
+  table.addEventListener('click', (e) => {
+    let thIndex:number;
+
+    tableHeaders.forEach((el, index) => {
+      if (el === e.target) thIndex = index;
+    });
+    if (thIndex === undefined) return;
+
+    const tBody = document.querySelector('tbody');
+    const colName:string = tableHeaders[thIndex].innerText;
+    const tableRows:NodeListOf<HTMLTableRowElement> = document.querySelectorAll('tr');
+    const isSorted:boolean = tableHeaders[thIndex].classList.contains('up-down');
+
+    const sortedRows:HTMLTableRowElement[] = Array.from(tableRows)
+      .slice(1)
+      .sort((a, b) => {
+        let result:number;
+        if (colName === 'Set' || colName === 'Word' || colName === 'Translation') {
+          result = a.cells[thIndex].innerHTML > b.cells[thIndex].innerHTML ? 1 : -1;
+        } else {
+          result = +a.cells[thIndex].innerHTML > +b.cells[thIndex].innerHTML ? 1 : -1;
+        }
+        if (isSorted) result *= -1;
+        return result;
+      });
+    tBody.append(...sortedRows);
+    tableHeaders[thIndex].classList.toggle('up-down');
+    tableHeaders.forEach((el, index) => {
+      if (thIndex !== index) el.classList.remove('up-down');
+    });
+  });
+}
+
 export function generateStatsPage() {
   const rootDiv: HTMLElement = getRootElement();
   const rowsArray:string[] = [];
@@ -13,12 +50,12 @@ export function generateStatsPage() {
     collection.forEach((card, cardIndex) => {
       const { clicksCount, wrongCount, rightCount } = state.logData[colIndex][cardIndex];
       const percent = (wrongCount + rightCount) === 0
-        ? 0 : (rightCount * 100) / (wrongCount + rightCount);
+        ? 0 : Math.round((rightCount * 100) / (wrongCount + rightCount));
       counter += 1;
       rowsArray.push(`
         <tr>
           <td>${counter}</td><td>${set}</td><td>${card.word}</td><td>${card.translation}</td>
-          <td>${clicksCount}</td><td>${rightCount}</td><td>${wrongCount}</td><td>${percent}%</td>
+          <td>${clicksCount}</td><td>${rightCount}</td><td>${wrongCount}</td><td>${percent}</td>
         </tr>
       `);
     });
@@ -32,6 +69,8 @@ export function generateStatsPage() {
       ${rowsArray.join('')}
     </table>
   `;
+
+  addEventListeners();
 }
 
 export function doNothing() {
